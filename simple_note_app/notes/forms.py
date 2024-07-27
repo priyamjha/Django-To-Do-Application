f17b108f7d7211374769934356a73bd976e24481
Priyam Jha <93506332+priyamjha@users.noreply.github.com>
2024-07-28 02:17:31 +0530

    simple_note_app

--- simple_note_app/notes/forms.py
+++
@@ -1,9 +0,0 @@
-# notes/forms.py
-
-from django import forms
-from .models import Note
-
-class NoteForm(forms.ModelForm):
-    class Meta:
-        model = Note
-        fields = ['title', 'content']
